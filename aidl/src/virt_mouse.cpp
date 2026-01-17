f3d79fabbc444fdf8f0699224ad1a34c2fd0d7b6
Thomas Makin <halorocker89@gmail.com>
2026-01-17 03:20:04 +0000

    [WIP] virt_mouse: fix stuttering
    Change-Id: I38708bcfa9aae22710179f812654c810c3f4278e

--- aidl/src/virt_mouse.cpp
+++ aidl/src/virt_mouse.cpp
@@ -117,27 +117,27 @@ void *virt_mouse::__mouseLoop(void *args) {
     virt_mouse *const self = static_cast<virt_mouse *>(args);
 
     while (self->ready.load()) {
-        // if we are writing the same values, don't
-        if (_axis_x != self->axis_x.load() || _axis_y != self->axis_y.load()) {
+        float _axis_x_update = self->axis_x.load();
+        float _axis_y_update = self->axis_y.load();
 
-            _axis_x = self->axis_x.load();
-            _axis_y = self->axis_y.load();
+        if (std::fabsf(_axis_x_update) <
+                std::stof(GetProperty(PROP_DEAD_X, DEFAULT_DEAD_X)) ||
+            std::fabsf(_axis_y_update) <
+                std::stof(GetProperty(PROP_DEAD_Y, DEFAULT_DEAD_Y))) {
+            _axis_x_update = 0;
+            _axis_y_update = 0;
+        }
 
-            // write value if x or y value is past dead zone else write 0
-            if (std::fabsf(_axis_x) >
-                    std::stof(GetProperty(PROP_DEAD_X, DEFAULT_DEAD_X)) ||
-                std::fabsf(_axis_y) >
-                    std::stof(GetProperty(PROP_DEAD_Y, DEFAULT_DEAD_Y))) {
-                libevdev_uinput_write_event(self->uidev, EV_REL, REL_X,
-                                            _axis_x);
-                libevdev_uinput_write_event(self->uidev, EV_REL, REL_Y,
-                                            _axis_y);
-            } else {
-                libevdev_uinput_write_event(self->uidev, EV_REL, REL_X, 0);
-                libevdev_uinput_write_event(self->uidev, EV_REL, REL_Y, 0);
-            }
+        // don't infinitely write 0;
+        if (_axis_x != 0 && _axis_y != 0 && _axis_x_update != 0 && _axis_y_update != 0) {
+            _axis_x = _axis_x_update;
+            _axis_y = _axis_y_update;
 
-            // send SYN report regardless
+            libevdev_uinput_write_event(self->uidev, EV_REL, REL_X,
+                                        _axis_x);
+            libevdev_uinput_write_event(self->uidev, EV_REL, REL_Y,
+                                        _axis_y);
+
             libevdev_uinput_write_event(self->uidev, EV_SYN, SYN_REPORT, 0);
         }
 
